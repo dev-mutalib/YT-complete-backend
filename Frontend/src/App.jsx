45e9b0e5c0769a6e42e0a296563d3123801c0e16
Abdul Mutalib <mutalib92847@gmail.com>
2026-06-28 18:35:16 +0530

    feat(App.jsx): Implement react-router-dom in App.jsx with home(/) path.

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path='/'
+          element={<h1>Hello World</h1>}
+        />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
